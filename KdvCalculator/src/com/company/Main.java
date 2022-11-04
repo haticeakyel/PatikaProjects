4046df4e112c249b6da2f963553d4761758a2cd2
Hatice Akyel <56586646+haticeakyel@users.noreply.github.com>
2022-11-04 12:25:24 +0300

    edit kdvCalculator

--- KdvCalculator/src/com/company/Main.java
+++ KdvCalculator/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        double tutar;
+        double kdv = 0.18;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen ucreti giriniz: ");
+        tutar = scanner.nextDouble();
+        double kdvTutar = tutar * kdv;
+        double toplamTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz tutar: " + tutar);
+        System.out.println("KDV Oranı: " + kdv);
+        System.out.println("KDV Tutarı: " + kdvTutar);
+        System.out.println("KDV'li Tutar: " + toplamTutar);
+
+
+
     }
 }
