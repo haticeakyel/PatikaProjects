37724ee5da392d364ca13d72528228158774340d
Hatice Akyel <56586646+haticeakyel@users.noreply.github.com>
2022-11-04 22:39:28 +0300

    make project work

--- Hipotenus/src/com/company/Main.java
+++ Hipotenus/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Üçgenin birinci kenar uzunluğunu giriniz: ");
+        int a = scanner.nextInt();
+        System.out.println("Üçgenin ikinci kenar uzunluğunu giriniz: ");
+        int b = scanner.nextInt();
+        double c = Math.sqrt((a*a) + (b*b));
+        System.out.println("Hipotenus: " +c);
+
+
     }
 }
