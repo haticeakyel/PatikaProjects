ef3cd37e67c664bb2f4236212241f66568a73b9d
Hatice Akyel <56586646+haticeakyel@users.noreply.github.com>
2022-11-04 23:00:05 +0300

    calculates body mass index

--- BodyMassIndex/src/com/company/Main.java
+++ BodyMassIndex/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        double boy = scanner.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz: ");
+        int kg = scanner.nextInt();
+        double index = kg / (boy * boy);
+        System.out.println("Vücut Kitle İndeksiniz: " + index);
+
     }
 }
