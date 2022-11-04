4813b02323fcd2b7afb1cb25fc5315939c59dadf
Hatice Akyel <56586646+haticeakyel@users.noreply.github.com>
2022-11-04 22:50:29 +0300

    calculates area of the circle

--- CircleArea/src/com/company/Main.java
+++ CircleArea/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code
+        Scanner scanner = new Scanner(System.in);
+        double pi = 3.14;
+        System.out.println("Dairenin yarıçapını giriniz: ");
+        int r = scanner.nextInt();
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Dairenin alanı: " + alan);
+        System.out.println("Dairenin cevresi: " + cevre);
     }
 }
