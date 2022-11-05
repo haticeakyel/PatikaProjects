8a9690264d25d2715d1733839859e4b3fe0de8a4
Hatice Akyel <56586646+haticeakyel@users.noreply.github.com>
2022-11-05 21:36:06 +0300

    take input from user and calculates total amount

--- GroceryStore/src/com/company/Main.java
+++ GroceryStore/src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        double armut = 2.14;
+        double elma = 3.67;
+        double domates = 1.11;
+        double muz = 0.95;
+        double patlican = 5.00;
+
+        final DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.println("Armut Kaç Kilo ? :");
+        double armutK = scanner.nextDouble();
+        System.out.println("Elma Kaç Kilo ? :");
+        double elmaK = scanner.nextDouble();
+        System.out.println("Domates Kaç Kilo ? :");
+        double domatesK = scanner.nextDouble();
+        System.out.println("Muz Kaç Kilo ? :");
+        double muzK = scanner.nextDouble();
+        System.out.println("Patlıcan Kaç Kilo ? :");
+        double patlicanK = scanner.nextDouble();
+
+        double toplam = (armut * armutK) + (elma * elmaK) + (domates * domatesK) + (muz * muzK) + (patlican * patlicanK);
+
+        System.out.println("Toplam tutar: " + df.format(toplam));
     }
 }
