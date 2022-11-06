3708a36fb24b8f2e549d8a0b8a078c1e73bdf755
Hatice Akyel <56586646+haticeakyel@users.noreply.github.com>
2022-11-06 23:11:41 +0300

    made calculator with switch-case

--- Calculator/src/com/company/Main.java
+++ Calculator/src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int x1, x2, choice;
+        System.out.println("Please enter first number: ");
+        x1 = scanner.nextInt();
+        System.out.println("Please enter second number: ");
+        x2 = scanner.nextInt();
+        System.out.println("1.Adding\n2.Subtraction\n3.Multiply\n4.Divide\n");
+        System.out.println("Please enter number to select your operation: ");
+        choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Result is: " + (x1 + x2));
+                break;
+            case 2:
+                System.out.println("Result is: " + (x1 - x2));
+                break;
+            case 3:
+                System.out.println("Result is: " + (x1 * x2));
+                break;
+            case 4:
+                if (x2 == 0){
+                    System.out.println("You can't divide by 0");
+                }
+                else{
+                    System.out.println("Result is: " + (x1 / x2));
+                }
+                break;
+            default:
+                System.out.println("You made something wrong");
+        }
+
     }
 }
